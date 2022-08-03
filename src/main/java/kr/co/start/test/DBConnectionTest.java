5ef63e73bf5526a9a10d282606313ecb4d7d7a6f
skdn4879 <skanxkrl3645@naver.com>
2022-08-03 11:09:15 +0900

    connection test complete

--- src/main/java/kr/co/start/test/DBConnectionTest.java
+++ src/main/java/kr/co/start/test/DBConnectionTest.java
@@ -1,10 +1,20 @@
 package kr.co.start.test;
 
+import java.sql.Connection;
+
+import kr.co.start.util.DBConnector;
+
 public class DBConnectionTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			Connection con = DBConnector.getConnection();
+			System.out.println(con != null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
