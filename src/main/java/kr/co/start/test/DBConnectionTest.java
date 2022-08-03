049850263ede60872e4e978ec666f62e0e134888
skdn4879 <skanxkrl3645@naver.com>
2022-08-03 10:40:28 +0900

    init

---
+++ src/main/java/kr/co/start/test/DBConnectionTest.java
@@ -0,0 +1,10 @@
+package kr.co.start.test;
+
+public class DBConnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/kr/co/start/test/TestMain.java
@@ -0,0 +1,11 @@
+package kr.co.start.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java!");
+		
+	}
+
+}
